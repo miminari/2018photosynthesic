8da8962f856d3cb3daa040821508c36dcbefe728
miminari <mimitips@gmail.com>
2018-10-05 09:19:45 +0900

    js: 画像の色取得（途中）

--- functions.php
+++ functions.php
@@ -82,6 +82,7 @@ function myTemplate_scripts() {
     //wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/vender/modernizr-2.8.3.min.js', array(),'2.8.3',false);
     wp_enqueue_script( 'vue', get_template_directory_uri() . '/js/vender/vue.js', array(), '2.5.16', true);
     wp_enqueue_script( 'axios', 'https://unpkg.com/axios/dist/axios.min.js', array(), '', true);
+    wp_enqueue_script( 'rgbaster', get_template_directory_uri() . '/js/vender/rgbaster.js', array(), '1.1.0', true);
     //wp_enqueue_script( 'rgbaster', get_template_directory_uri() . '/js/vender/rgbaster.min.js', array(), '1.0.0', false);
     //デバック時と本番で切り替えるもの
     if(SCRIPT_DEBUG) {
@@ -149,4 +150,61 @@ function svg_func( $atts, $content = "" )  {
     return '<span class="m-svg--'.$content.'"><svg><use xlink:href="#'.$content.'"></use></svg></span>';
 }
 add_shortcode( 'svg', 'svg_func' );
+
+/*********************
+OGPタグ/Twitterカード設定を出力
+*********************/
+function my_meta_ogp() {
+  if( is_front_page() || is_home() || is_singular() ){
+    global $post;
+    $ogp_title = '';
+    $ogp_descr = '';
+    $ogp_url = '';
+    $ogp_img = '';
+    $insert = '';
+
+    if( is_singular() ) { //記事＆固定ページ
+       setup_postdata($post);
+       $ogp_title = $post->post_title;
+       $ogp_descr = mb_substr(get_the_excerpt(), 0, 100);
+       $ogp_url = get_permalink();
+       wp_reset_postdata();
+    } elseif ( is_front_page() || is_home() ) { //トップページ
+       $ogp_title = get_bloginfo('name');
+       $ogp_descr = get_bloginfo('description');
+       $ogp_url = home_url();
+    }
+
+    //og:type
+    $ogp_type = ( is_front_page() || is_home() ) ? 'website' : 'article';
+
+    //og:image
+    if ( is_singular() && has_post_thumbnail() ) {
+       $ps_thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full');
+       $ogp_img = $ps_thumb[0];
+    } else {
+     $ogp_img = 'http://blog.photosynthesic.jp/wordpress/wp-content/uploads/2017/01/top_photo-3-768x432.jpg';
+    }
+
+    //出力するOGPタグをまとめる
+    $insert .= '<meta property="og:title" content="'.esc_attr($ogp_title).'" />' . "\n";
+    $insert .= '<meta property="og:description" content="'.esc_attr($ogp_descr).'" />' . "\n";
+    $insert .= '<meta property="og:type" content="'.$ogp_type.'" />' . "\n";
+    $insert .= '<meta property="og:url" content="'.esc_url($ogp_url).'" />' . "\n";
+    $insert .= '<meta property="og:image" content="'.esc_url($ogp_img).'" />' . "\n";
+    $insert .= '<meta property="og:site_name" content="'.esc_attr(get_bloginfo('name')).'" />' . "\n";
+    $insert .= '<meta name="twitter:card" content="summary_large_image" />' . "\n";
+    $insert .= '<meta name="twitter:site" content="photosynthesic" />' . "\n";
+    $insert .= '<meta property="og:locale" content="ja_JP" />' . "\n";
+
+    //facebookのapp_id（設定する場合）
+    $insert .= '<meta property="fb:app_id" content="450826578664610">' . "\n";
+    //app_idを設定しない場合ここまで消す
+
+    echo $insert;
+  }
+} //END my_meta_ogp
+
+add_action('wp_head','my_meta_ogp');//headにOGPを出力
+
 ?>
