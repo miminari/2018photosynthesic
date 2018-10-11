4e887c988455df16e821f763a232b26b8492d4aa
miminari <mimitips@gmail.com>
2018-10-11 20:06:10 +0900

    php: 404ページ作成

--- 404.php
+++ 404.php
@@ -1,1 +1,10 @@
-404
+<?php get_header(); ?>
+<div class="l-contanier">
+    <main class="l-main">
+    	<?php get_template_part( 'template-parts/post/no', get_post_format() ); ?>
+    </main>
+    <aside class="l-aside">
+        <?php get_sidebar(); ?>
+    </aside>
+</div>
+<?php get_footer(); ?>
---
+++ template-parts/post/no.php
@@ -0,0 +1,9 @@
+<article class="m-article">
+	<header class="m-article__header">
+		<h1 class="m-ttl">404 Not Found</h1>
+	</header>
+	<div class="m-article__body">
+		<h2>お探しのページは見つかりませんでした。</h2>
+		<p>あなたがアクセスしようとしたページは削除されたかURLが変更されているため、見つけることができません。</p>
+	</div>
+</article><!-- #post-## -->
