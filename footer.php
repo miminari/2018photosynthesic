e6d6f6f8558e3a8e4ec8542826514db532d5fa64
miminari <mimitips@gmail.com>
2018-10-27 10:14:13 +0900

    style: change font-family, and test css animation (wip)

--- footer.php
+++ footer.php
@@ -3,6 +3,40 @@
 </footer>
 
 </div><!-- end .l-warpper -->
+<div class="l-backscreen">
+    <div class="m-shape--asanoha">
+        <span class="m-shape--asanoha__part">
+            <span class="m-shape--triangle"></span>
+            <span class="m-shape--triangle"></span>
+            <span class="m-shape--triangle"></span>
+        </span>
+        <span class="m-shape--asanoha__part">
+            <span class="m-shape--triangle"></span>
+            <span class="m-shape--triangle"></span>
+            <span class="m-shape--triangle"></span>
+        </span>
+        <span class="m-shape--asanoha__part">
+            <span class="m-shape--triangle"></span>
+            <span class="m-shape--triangle"></span>
+            <span class="m-shape--triangle"></span>
+        </span>
+        <span class="m-shape--asanoha__part">
+            <span class="m-shape--triangle"></span>
+            <span class="m-shape--triangle"></span>
+            <span class="m-shape--triangle"></span>
+        </span>
+        <span class="m-shape--asanoha__part">
+            <span class="m-shape--triangle"></span>
+            <span class="m-shape--triangle"></span>
+            <span class="m-shape--triangle"></span>
+        </span>
+        <span class="m-shape--asanoha__part">
+            <span class="m-shape--triangle"></span>
+            <span class="m-shape--triangle"></span>
+            <span class="m-shape--triangle"></span>
+        </span>
+    </div>
+</div>
 <?php wp_footer();
 get_template_part( 'template-parts/global/svgsprite', get_post_format() ); ?>
 </body>
--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 </script>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="">
+<meta name="description" content="<?php bloginfo('description'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php wp_head(); ?>
 </head>
